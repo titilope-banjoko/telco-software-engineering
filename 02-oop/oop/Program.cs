d0290ccff7cb6d94024ff9a045d9109590f5561d
Titilope Banjoko <titilopebanjoko@outlook.com>
2026-09-23 20:36:50 +0100

    Add OOP Prj

---
+++ 02-oop/oop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            User usr = new User();
+        }
+    }
+}
