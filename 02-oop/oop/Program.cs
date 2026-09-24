cf31febc65a2334473a31d14ce8565256a891db9
Titilope Banjoko <titilopebanjoko@outlook.com>
2026-09-24 11:47:10 +0100

    Class

--- 02-oop/oop/Program.cs
+++ 02-oop/oop/Program.cs
@@ -7,7 +7,25 @@ namespace oop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
             User usr = new User();
+            // User = class and datatype
+            // usr = object
+            // User usr = new User(); = instance of a class
+            usr.firstName = "Titilope";
+            usr.lastName = "Banjoko";
+            usr.age = 17;
+            usr.complexion = "Brown";
+
+            User usr1 = new User();
+
+            User usr2 = new User();
+
+            Console.WriteLine("First Name: " + usr.firstName);
+            Console.WriteLine("Last Name: " + usr.lastName);
+            Console.WriteLine("Age: " + usr.age);
+
+            Console.ReadLine();
         }
     }
 }
--- 02-oop/oop/User.cs
+++ 02-oop/oop/User.cs
@@ -8,6 +8,10 @@ namespace oop
 {
     internal class User
     {
+        // This is a specification or blueprint otherwise known as Class 
         internal string firstName;
+        internal string lastName;
+        internal string complexion;
+        internal int age;
     }
 }
